ac0a8c058a08d97963c6b8f5b7b0c8eab19ae210
Alena Alex <alena26911@rambler.ru>
2023-02-13 19:28:44 +0300

    dz10

---
+++ data.js
@@ -0,0 +1,131 @@
+const dataJson = `[
+    {
+      "id": 49255,
+      "answer": "Glass",
+      "question": "A vitrine is a cabinet with doors made of this material",
+      "value": 400,
+      "airdate": "1996-12-05T20:00:00.000Z",
+      "created_at": "2022-12-30T18:58:23.188Z",
+      "updated_at": "2022-12-30T18:58:23.188Z",
+      "category_id": 582,
+      "game_id": 586,
+      "img": "https://sc02.alicdn.com/kf/Hfd66f1c499e4422fb640c3974b4a49517/618713/Hfd66f1c499e4422fb640c3974b4a49517.jpg",
+      "invalid_count": null,
+      "category": {
+        "id": 582,
+        "title": "furniture",
+        "created_at": "2022-12-30T18:39:10.176Z",
+        "updated_at": "2022-12-30T18:39:10.176Z",
+        "clues_count": 146
+        
+      }
+    },
+    {
+      "id": 93389,
+      "answer": "Sui",
+      "question": "The last name of designer Anna, it sounds like a popular hog call",
+      "value": 1000,
+      "airdate": "2004-04-26T19:00:00.000Z",
+      "created_at": "2022-12-30T19:20:23.757Z",
+      "updated_at": "2022-12-30T19:20:23.757Z",
+      "category_id": 9755,
+      "img":"https://pro-dachnikov.com/uploads/posts/2023-01/1673062439_pro-dachnikov-com-p-dizainer-interera-foto-5.jpg",
+      "game_id": 3182,
+      "invalid_count": null,
+      "category": {
+        "id": 9755,
+        "title": "also a chinese dynasty",
+        "created_at": "2022-12-30T19:20:23.403Z",
+        "updated_at": "2022-12-30T19:20:23.403Z",
+        "clues_count": 5
+        
+
+      }
+    },
+    {
+        "id": 49255,
+        "answer": "Glass",
+        "question": "A vitrine is a cabinet with doors made of this material",
+        "value": 400,
+        "airdate": "1996-12-05T20:00:00.000Z",
+        "created_at": "2022-12-30T18:58:23.188Z",
+        "updated_at": "2022-12-30T18:58:23.188Z",
+        "category_id": 582,
+        "game_id": 586,
+        "img": "https://sc02.alicdn.com/kf/Hfd66f1c499e4422fb640c3974b4a49517/618713/Hfd66f1c499e4422fb640c3974b4a49517.jpg",
+        "invalid_count": null,
+        "category": {
+          "id": 582,
+          "title": "furniture",
+          "created_at": "2022-12-30T18:39:10.176Z",
+          "updated_at": "2022-12-30T18:39:10.176Z",
+          "clues_count": 146
+          
+        }
+      },
+      {
+        "id": 93389,
+        "answer": "Sui",
+        "question": "The last name of designer Anna, it sounds like a popular hog call",
+        "value": 1000,
+        "airdate": "2004-04-26T19:00:00.000Z",
+        "created_at": "2022-12-30T19:20:23.757Z",
+        "updated_at": "2022-12-30T19:20:23.757Z",
+        "category_id": 9755,
+        "img":"https://pro-dachnikov.com/uploads/posts/2023-01/1673062439_pro-dachnikov-com-p-dizainer-interera-foto-5.jpg",
+        "game_id": 3182,
+        "invalid_count": null,
+        "category": {
+          "id": 9755,
+          "title": "also a chinese dynasty",
+          "created_at": "2022-12-30T19:20:23.403Z",
+          "updated_at": "2022-12-30T19:20:23.403Z",
+          "clues_count": 5
+          
+  
+        }
+      },
+      {
+        "id": 49255,
+        "answer": "Glass",
+        "question": "A vitrine is a cabinet with doors made of this material",
+        "value": 400,
+        "airdate": "1996-12-05T20:00:00.000Z",
+        "created_at": "2022-12-30T18:58:23.188Z",
+        "updated_at": "2022-12-30T18:58:23.188Z",
+        "category_id": 582,
+        "game_id": 586,
+        "img": "https://sc02.alicdn.com/kf/Hfd66f1c499e4422fb640c3974b4a49517/618713/Hfd66f1c499e4422fb640c3974b4a49517.jpg",
+        "invalid_count": null,
+        "category": {
+          "id": 582,
+          "title": "furniture",
+          "created_at": "2022-12-30T18:39:10.176Z",
+          "updated_at": "2022-12-30T18:39:10.176Z",
+          "clues_count": 146
+          
+        }
+      },
+      {
+        "id": 93389,
+        "answer": "Sui",
+        "question": "The last name of designer Anna, it sounds like a popular hog call",
+        "value": 1000,
+        "airdate": "2004-04-26T19:00:00.000Z",
+        "created_at": "2022-12-30T19:20:23.757Z",
+        "updated_at": "2022-12-30T19:20:23.757Z",
+        "category_id": 9755,
+        "img":"https://pro-dachnikov.com/uploads/posts/2023-01/1673062439_pro-dachnikov-com-p-dizainer-interera-foto-5.jpg",
+        "game_id": 3182,
+        "invalid_count": null,
+        "category": {
+          "id": 9755,
+          "title": "also a chinese dynasty",
+          "created_at": "2022-12-30T19:20:23.403Z",
+          "updated_at": "2022-12-30T19:20:23.403Z",
+          "clues_count": 5
+          
+  
+        }
+      }
+  ]`;
